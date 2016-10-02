6bf85fa2e651860af6ec9297686d63dc8504d01e
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 15:38:59 +0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] m = new int[8] { 12, 2, 5, 8, -6, 7, 5, 7 };
+            int a = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (m[i] > 0)
+                {
+                    a += (m[i] * m[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
