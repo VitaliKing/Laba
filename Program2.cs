b4fca8b9c875751acb0533d65663aa2cb3144c09
Vitalik <Vitalik.Trofimenko.1998@mail.ru>
2016-10-02 15:38:51 +0700

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] m = new int[8] { -15, 28, 3, 0, -6, 10, -5, -20 };
+            int min = 0;
+            int a = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (m[i]<min)
+                {
+                    min = m[i];
+                    a = i;
+                }
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
